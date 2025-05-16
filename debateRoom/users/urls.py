1388dd236fb4e9dac6ffb0ca1af281c0b79a19b3
N.Vasundhara Rani <nayakalluvasundhara483@gmail.com>
2025-05-16 23:03:21 +0530

    user authen

--- debateRoom/users/urls.py
+++ debateRoom/users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views #to access views.py from the current directory
 
 urlpatterns = [
-    
+    path("",views.index, name="index"),
+    path("login/",views.login_view,name="login"),
+    path("logout/",views.logout_view,name="logout"),
+    path("register/",views.register_view,name="register")
 ]
--- debateRoom/users/views.py
+++ debateRoom/users/views.py
@@ -1,3 +1,54 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirm = request.POST["confirm"]
+
+        if password != confirm:
+            return render(request, "users/register.html", {
+                "message" : "Passwords do not match"
+            })
+
+        try:
+            user = User.objects.create_user(username,email,password)
+            user.save()
+        except:
+            return render(request, "users/register.html", {
+                "message" : "Username is already taken"
+            })
+        login(request,user)
+        return HttpResponseRedirect(reverse("index"))
+    return render(request,"users/register.html")
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password = password)
+        if user is not None:
+            login(request,user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request,"users/login.html", {
+                "message" : "Invalid Credentials"
+            })
+    return render(request,"users/login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {
+        "message":"Logged out"
+    })
